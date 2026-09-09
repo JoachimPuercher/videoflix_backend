097eef26fcc76aabdf0f3cd47cdd5a5822a35e21
JoachimPuercher <joachim.puercher@gmail.com>
2026-09-09 20:14:24 +0200

    chore(auth): drop unused token generator import from tasks
    The token is now generated in the registration view and passed in,
    so tasks.py no longer needs default_token_generator.

--- auth_app/tasks.py
+++ auth_app/tasks.py
@@ -3,7 +3,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 import os
 from email.message import MIMEPart
-from django.contrib.auth.tokens import default_token_generator
+
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.conf import settings
